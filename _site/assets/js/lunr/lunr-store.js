9f007926966ef4b20f025993b097fc5940ba0751
Bill Rawlinson <bill.rawlinson@mheducation.com>
2024-11-18 17:15:42 -0500

    dont commit _site

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Starting Fresh",
-        "excerpt":"What’s Up? My blog here was looking like complete shit. The theme had broken and I am not really sure how to fix it so I’m starting over. Most of my old posts may never come back but we’ll see. For the most part I’m not sure they had that...","categories": ["blogging"],
-        "tags": ["personal"],
-        "url": "/blogging/2024/11/16/starting-fresh.html",
-        "teaser": null
-      }]
